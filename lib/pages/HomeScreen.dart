0db49ca318368d5de4b0757c228fec4208c24125
lennykmacedo <lennykmacedo@users.noreply.github.com>
2020-02-17 18:18:31 -0300

    first commit

--- lib/pages/HomeScreen.dart
+++ lib/pages/HomeScreen.dart
@@ -105,7 +105,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     text: [
                       "Lennyk Macedo",
                       "Developer",
-                      "Flutter Mobile and",
+                      "Mobile and",
                       "Front-End"
                     ],
                   ),
